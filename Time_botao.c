8f7b131d543644ce64afc9abea731b7bdc06c8ec
Rayane Queiroz <rayaneqsp2@gmail.com>
2025-02-05 21:26:31 -0300

    ajustando ligação dos pinos

--- Time_botao.c
+++ Time_botao.c
@@ -45,14 +45,14 @@ void update_leds() {
             gpio_put(LED_GREEN_PIN, 1);
             break;
         case TWO_LEDS_ON:
-            gpio_put(LED_BLUE_PIN, 0);
+            gpio_put(LED_BLUE_PIN, 1);
             gpio_put(LED_RED_PIN, 1);
-            gpio_put(LED_GREEN_PIN, 1);
+            gpio_put(LED_GREEN_PIN, 0);
             break;
         case ONE_LED_ON:
             gpio_put(LED_BLUE_PIN, 0);
-            gpio_put(LED_RED_PIN, 0);
-            gpio_put(LED_GREEN_PIN, 1);
+            gpio_put(LED_RED_PIN, 1);
+            gpio_put(LED_GREEN_PIN, 0);
             break;
         case ALL_LEDS_OFF:
             gpio_put(LED_BLUE_PIN, 0);
